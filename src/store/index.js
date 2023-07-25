fbbeda6185ee262eb27e7981fffff81f2af7951e
Renato Cipriano <renato.cipriano.dev@gmail.com>
2023-07-25 18:49:02 -0300

    config vuex persistence

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
 import games from "./games";
-import { createStore } from 'vuex';
-const store = createStore(games);
-export default store;
+import createStore from 'vuex'
+const store = new createStore.Store(games)
+export default store
