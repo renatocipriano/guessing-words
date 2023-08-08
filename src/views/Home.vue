45a1641e32a8bb29bbebdccbd588789c4e16e711
Renato Cipriano <renato.cipriano.dev@gmail.com>
2023-08-07 21:30:35 -0300

    prevent new guess when game is completed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,19 @@
   <FormNewGameVue />
   <FormGuess v-if="game != null" />
   <TableGuess v-if="game != null" />
+  <Congratulations v-if="isCompleted" />
 </template>
 
 <script>
 import FormGuess from "@/components/FormGuess.vue";
 import FormNewGameVue from "@/components/FormNewGame.vue";
 import TableGuess from "@/components/TableGuess.vue";
+import Congratulations from "@/components/Congratulations.vue";
 import { commonComputed } from "@/store/games/common";
 
 export default {
   name: "HomePage",
-  components: { FormNewGameVue, FormGuess, TableGuess },
+  components: { FormNewGameVue, FormGuess, TableGuess, Congratulations },
   computed: {
     ...commonComputed,
   },
